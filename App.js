5199ee895defbdb5af8d59c6a8811cde4fd8706a
Maulana Haikal <maulana.haikal.98@gmail.com>
2020-11-29 23:48:02 +0800

    import new screen to app js

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { SplashScreen } from './src/pages/SplashScreen'
+import { LoginScreen } from './src/pages/LoginScreen'
+import { RegisterScreen } from './src/pages/RegisterScreen'
 
 export default function App() {
   return (
     <>
       <StatusBar style="auto" />
-			<SplashScreen />
+			<RegisterScreen />
     </>
   );
 }
