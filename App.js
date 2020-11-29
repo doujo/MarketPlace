713c4291c4a97cb35372039b4d3f6442aab4421d
Maulana Haikal <maulana.haikal.98@gmail.com>
2020-11-30 00:15:27 +0800

    update app js

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { SplashScreen } from './src/pages/SplashScreen'
+import { WelcomeScreen } from './src/pages/WelcomeScreen'
 import { LoginScreen } from './src/pages/LoginScreen'
 import { RegisterScreen } from './src/pages/RegisterScreen'
 
@@ -9,7 +10,7 @@ export default function App() {
   return (
     <>
       <StatusBar style="auto" />
-			<RegisterScreen />
+			<WelcomeScreen />
     </>
   );
 }
