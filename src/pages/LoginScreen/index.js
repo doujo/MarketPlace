e9962fec6d3988f95ad5141669476976fe10ae05
Maulana Haikal <maulana.haikal.98@gmail.com>
2020-11-29 23:47:24 +0800

    setup login screen layout

---
+++ src/pages/LoginScreen/index.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import {
+	View,
+	Text,
+	TextInput,
+	TouchableOpacity,
+	ImageBackground, Image,
+	StatusBar
+} from 'react-native';
+
+import { styles } from './style.js'
+const backgroundImage = require('../../../assets/images/background3.png');
+
+export const LoginScreen = () => {
+	return(
+		<View style={styles.mainContainer}>
+			<Image
+				source={backgroundImage}
+				style={styles.backgroundContainer}/>
+			<View style={styles.loginCardContainer}>
+				<View style={styles.cardContainer}>
+					<Text style={styles.textTitle}>Sign In</Text>
+				</View>
+				<View style={styles.formContainer}>
+					<View style={styles.formGroup}>
+						<TextInput
+							style={styles.formInput}
+							placeholder="Username"
+						/>
+					</View>
+					<View style={styles.formGroup}>
+						<TextInput
+							style={styles.formInput}
+							type='password'
+							placeholder="Password"
+						/>
+					</View>
+					<View style={styles.formGroup}>
+						<TouchableOpacity
+							style={styles.btnPrimary}
+						>
+							Login
+						</TouchableOpacity>
+					</View>
+				</View>
+				<View style={styles.secondaryActionContainer}>
+					<Text style={styles.secondaryActionText}>
+						Forgot Password ?
+					</Text>
+					<TouchableOpacity style={styles.secondaryActionButton}>
+						Sign up
+					</TouchableOpacity>
+				</View>
+			</View>
+		</View>
+	)
+}
---
+++ src/pages/LoginScreen/style.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import {
+	StyleSheet
+} from 'react-native';
+
+const backgroundImage = '../../../assets/images/background2.png'
+
+export const styles = StyleSheet.create({
+	mainContainer: {
+		flexGrow: 1,
+		flexDirection: 'column',
+		alignItems: 'flex-end'
+	},
+	backgroundContainer: {
+		flex: 3,
+		alignSelf: 'start',
+    width: '100%',
+  },
+	loginCardContainer: {
+		flex: 3,
+		alignSelf: 'end',
+		top: -48,
+		width: '100%',
+    paddingTop: 62,
+		paddingHorizontal: 32,
+    borderTopLeftRadius: 50,
+    borderTopRightRadius: 50,
+    backgroundColor: "#fff",
+    elevation: 6,
+		fontFamily: 'SF Pro Display',
+	},
+	textTitle: {
+		fontSize: 36,
+		fontWeight: 'bold',
+		marginBottom: 16
+	},
+	formGroup: {
+		marginVertical: 20,
+	},
+	formLabel: {
+		fontSize: 18,
+		marginBottom: 8
+	},
+	formInput: {
+		height: 48,
+		fontSize: 16,
+		borderColor: '#828282',
+		color: '#C4C4C4',
+		borderBottomWidth: 1,
+		paddingVertical: 10,
+	},
+	btnPrimary: {
+		height: 44,
+		fontFamily: 'Inter',
+		fontWeight: 500,
+		backgroundColor: '#FD4D4D',
+		borderRadius: 16,
+		paddingHorizontal: 24,
+		paddingVertical: 12,
+		color: '#fff',
+		fontSize: 18,
+		textAlign: 'center'
+	},
+	secondaryActionContainer: {
+		marginTop: 8,
+		flexDirection: 'row',
+		justifyContent: 'space-between'
+	},
+	secondaryActionText: {
+		fontSize: 15,
+		color: '#FD4D4D',
+		fontWeight: 600,
+	},
+	secondaryActionButton: {
+		fontSize: 15,
+		fontWeight: 600,
+	}
+})
